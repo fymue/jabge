58dd0a2852fd3a6d4c5b56d160ac8bcd92a20cf4
Fynn Muermanns <fynn.m@live.de>
2023-05-08 01:29:03 +0200

    add basic vector functions

--- engine/src/types.cpp
+++ engine/src/types.cpp
@@ -3,4 +3,24 @@
 
 namespace engine {
 
+template<typename T = float>
+inline std::ostream &operator<<(std::ostream &stream, Vec2<T> vec) {
+  stream << "Vec2{" << vec.x << ", " << vec.y << "} ";
+  return stream;
+}
+
+template<typename T = float>
+inline std::ostream &operator<<(std::ostream &stream, Vec3<T> vec) {
+  stream << "Vec3{" << vec.x << ", " << vec.y
+         << ", " << vec.z << "} ";
+  return stream;
+}
+
+template<typename T = float>
+inline std::ostream &operator<<(std::ostream &stream, Vec4<T> vec) {
+  stream << "Vec4{" << vec.w << ", " < vec.x
+         << ", " << vec.y << ", " << vec.z << "} ";
+  return stream;
+}
+
 }  // namespace engine
--- engine/src/types.h
+++ engine/src/types.h
@@ -14,7 +14,31 @@ struct PUB_API Vec2 {
 
   Vec2(T x, T y) :
     x(x), y(y) {}
+
+  inline T dot(Vec2 other) const {
+    return x * other.x + y * other.y;
+  }
+
+  inline T sum() const {
+    return x + y;
+  }
+
+  inline T dist(Vec2 other) const {
+  }
+
+  inline T len() const {
+    return std::sqrt(x * x + y * y);
+  }
+
+  // convert vector to unit vector
+  inline Vec2 norm() const {
+    T length = len();
+    return Vec2(x / length, y / length);
+  }
 };
+
+template<typename T = float>
+inline std::ostream &operator<<(std::ostream &stream, Vec2<T> vec);
 
 template<typename T = float>
 struct PUB_API Vec3 {
@@ -25,7 +49,31 @@ struct PUB_API Vec3 {
 
   Vec3(T x, T y, T z) :
     x(x), y(y), z(z) {}
+
+  inline T dot(Vec3 other) const {
+    return x * other.x + y * other.y + z * other.z;
+  }
+
+  inline T sum() const {
+    return x + y + z;
+  }
+
+  inline T dist(Vec3 other) const {
+  }
+
+  inline T len() const {
+    return std::sqrt(x * x + y * y + z * z);
+  }
+
+  // convert vector to unit vector
+  inline Vec3 norm() const {
+    T length = len();
+    return Vec3(x / length, y / length, z / length);
+  }
 };
+
+template<typename T = float>
+inline std::ostream &operator<<(std::ostream &stream, Vec3<T> vec);
 
 template<typename T = float>
 struct PUB_API Vec4 {
@@ -36,7 +84,30 @@ struct PUB_API Vec4 {
 
   Vec4(T w, T x, T y, T z) :
     w(w), x(x), y(y), z(z) {}
+
+  inline T dot(Vec4 other) const {
+    return w * other.w + x * other.x + y * other.y + z * other.z;
+  }
+  inline T sum() const {
+    w + x + y + z;
+  }
+
+  inline T dist(Vec4 other) const {
+  }
+
+  inline T len() const {
+    return std::sqrt(w * w + x * x + y * y + z * z);
+  }
+
+  // convert vector to unit vector
+  inline Vec4 norm() const {
+    T length = len();
+    return Vec4(w / length, x / length, y / length, z / length);
+  }
 };
+
+template<typename T = float>
+inline std::ostream &operator<<(std::ostream &stream, Vec4<T> vec);
 
 }  // namespace engine
 
